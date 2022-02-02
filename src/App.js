aa830b74d88bfb2df91541fefb8c6c68c6558064
grayTurtle01 <the.correo@protonmail.com>
2022-02-02 12:41:20 -0600

    AddTask function working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { nanoid } from 'nanoid'
 
 
@@ -7,25 +7,36 @@ import Form from './components/Form'
 import FilterButton from './components/FilterButton'
 import Task from './components/Task'
 
-let tasks = [ {text:"First task", completed:false},
+let data = [ {text:"First task", completed:false},
               {text:"Second task", completed:false} ]
 
 
 
 
 function App(props) {
+  // State
+  const [tasks, setTasks] = useState(data)
 
-  // Default Data
+
+  // TaskList for <ul>
   let taskList = tasks.map( (task) => (
     <Task text={task.text} key={nanoid()}/>
   ))
   
+  
+  // Functions
+  function addTask(text){
+    let newTask = {'text':text, completed:false}
+    setTasks( [...tasks, newTask])
+
+  }
+
 
   // return
   return (
    <div>
 
-    <Form />  
+    <Form addTask={addTask} />  
 
     <div>
       <FilterButton name='All' />
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,27 @@
-function Form(){
+import { useState } from 'react'
+ 
+function Form(props){
+  // state
+  const [newTask, setNewTask] = useState('')
+
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log(newTask)
+    setNewTask('')
+    props.addTask(newTask)
+  }
+
+  function handleOnChange(e){
+    setNewTask(e.target.value)
+  }
 
   return(
-  <form>
-    <input placeholder="Add New Task"/>
+  <form onSubmit={handleSubmit}>
+    <input placeholder="Add New Task" 
+           onChange={ handleOnChange }
+           value={newTask}
+           />
     <button>Add Task</button>
   </form>
   )
